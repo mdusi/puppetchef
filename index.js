5466f9647ee770a19e1aeeb8a2e85d47fd3e49b0
mdusi <mdusi@users.noreply.github.com>
2025-04-09 10:58:10 -0700

    bump: version 3.1.0 -> 3.2.0

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const program = new Command();
  */
 program
   .name('puppetchef')
-  .version('3.1.0')
+  .version('3.2.0')
   .description('Puppetchef CLI')
   .option('-c, --conf <file>', 'config file', 'puppetchefrc')
   .option('--syntax-check', 'validate recipe only', false)
